2900ee276b767f8fa1c5145b47ce1e768504419b
eotkd4791 <eotkd4791@gmail.com>
2022-01-27 16:08:15 +0900

    Add chap5

---
+++ spring5-beginner/daesang/src/main/java/chap5/DuplicateMemberException.java
@@ -0,0 +1,8 @@
+package chap5;
+
+public class DuplicateMemberException extends RuntimeException {
+	
+	public DuplicateMemberException(String message) {
+		super(message);
+	}
+}
--- spring5-beginner/daesang/src/main/java/config/AppCtx.java
+++ spring5-beginner/daesang/src/main/java/config/AppCtx.java
@@ -2,34 +2,16 @@ package config;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import chap4.ChangePasswordService;
-import chap4.MemberDao;
-import chap4.MemberInfoPrinter;
-import chap4.MemberListPrinter;
-import chap4.MemberPrinter;
-import chap4.MemberRegisterService;
-import chap4.MemberSummaryPrinter;
-import chap4.VersionPrinter;
+import chap5.MemberPrinter;
+import chap5.MemberSummaryPrinter;
+import chap5.VersionPrinter;
 
 @Configuration
+@ComponentScan(basePackages = { "chap5" })
 public class AppCtx {
-	
-	@Bean
-	public MemberDao memberDao() {
-		return new MemberDao();
-	}
-	
-	@Bean
-	public MemberRegisterService memberRegSvc() {
-		return new MemberRegisterService(memberDao());
-	}
-	
-	@Bean
-	public ChangePasswordService changePwdSvc() {
-		return new ChangePasswordService();
-	}
 	
 	@Bean
 	@Qualifier("printer")
@@ -41,16 +23,6 @@ public class AppCtx {
 	@Qualifier("summaryPrinter")
 	public MemberSummaryPrinter memberPrinter2() {
 		return new MemberSummaryPrinter();
-	}
-	
-	@Bean
-	public MemberListPrinter listPrinter() {
-		return new MemberListPrinter();
-	}
-	
-	@Bean
-	public MemberInfoPrinter infoPrinter() {
-		return new MemberInfoPrinter();
 	}
 	
 	@Bean
